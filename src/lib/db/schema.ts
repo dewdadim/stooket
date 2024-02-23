7c3e6e39bc44b369623b20f7e2f960747141eaf3
Nadim Hairi <nadimhairi@hotmail.com>
2024-02-23 21:41:16 +0800

    modify: schema.ts

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,19 +1,15 @@
-import { mysqlTable, text, varchar, int } from "drizzle-orm/mysql-core";
-
-/**
- * This is a sample schema.
- * Replace this with your own schema and then run migrations.
- */
+import { mysqlTable, text, varchar, int } from "drizzle-orm/mysql-core"
 
 export const user = mysqlTable("user", {
   id: int("id").autoincrement().primaryKey(),
-  fullName: text("full_name"),
+  username: varchar("username", { length: 256 }).unique(),
+  fullname: varchar("fullname", { length: 256 }),
   phone: varchar("phone", { length: 256 }),
-});
+})
 
 export const post = mysqlTable("post", {
   id: int("id").autoincrement().primaryKey(),
   title: text("title"),
   likes: int("likes"),
   userId: int("userId"),
-});
+})
