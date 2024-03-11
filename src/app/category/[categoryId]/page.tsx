68a3ad07ddd7740444d142b0d6de53c85f645499
Nadim Hairi <nadimhairi@hotmail.com>
2024-03-11 20:26:56 +0800

    fix function name

--- src/app/category/[categoryId]/page.tsx
+++ src/app/category/[categoryId]/page.tsx
@@ -1,6 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 
-export default function ProductDetails() {
+export default function Category() {
   return (
     <MaxWidthWrapper>
       <h1 className="mt-16 text-xl font-bold">CATEGORYYY</h1>
