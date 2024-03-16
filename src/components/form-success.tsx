fe9747a25c53cc1b5988e69fba15bf93c7f9bec9
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-03-16 22:25:31 +0800

    Update form-success.tsx

--- src/components/form-success.tsx
+++ src/components/form-success.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { CheckCircle2 } from "lucide-react"
 import { useToast } from "./ui/use-toast"
 
@@ -11,7 +9,7 @@ export const FormSuccess = ({ message }: FormSuccessProps) => {
   if (!message) return null
   const { toast } = useToast()
 
-  return toast({
+  toast({
     variant: "success",
     action: (
       <div className="flex w-full items-center">
@@ -20,6 +18,7 @@ export const FormSuccess = ({ message }: FormSuccessProps) => {
       </div>
     ),
   })
+  return
   // return (
   //   // <div className="flex items-center gap-x-2 rounded-md bg-emerald-500/15 p-3 text-sm text-emerald-500">
   //   //   <CheckCircle2 className="h-4 w-4" />
