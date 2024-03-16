44abeec4b57990e13a29b0f61d20753ac17b6372
Nadim Hairi <118960850+dewdadim@users.noreply.github.com>
2024-03-16 22:32:58 +0800

    Update form-success.tsx

--- src/components/form-success.tsx
+++ src/components/form-success.tsx
@@ -9,16 +9,19 @@ export const FormSuccess = ({ message }: FormSuccessProps) => {
   if (!message) return null
   const { toast } = useToast()
 
-  toast({
-    variant: "success",
-    action: (
-      <div className="flex w-full items-center">
-        <CheckCircle2 className="mr-2 h-4 w-4" />
-        <span className="first-letter:capitalize">{message}</span>
-      </div>
-    ),
-  })
-  return
+  return (
+    <>
+      {toast({
+        variant: "success",
+        action: (
+          <div className="flex w-full items-center">
+            <CheckCircle2 className="mr-2 h-4 w-4" />
+            <span className="first-letter:capitalize">{message}</span>
+          </div>
+        ),
+      })}
+    </>
+  )
   // return (
   //   // <div className="flex items-center gap-x-2 rounded-md bg-emerald-500/15 p-3 text-sm text-emerald-500">
   //   //   <CheckCircle2 className="h-4 w-4" />
