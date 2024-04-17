1af89ffb32d75eed6c0a907d356caee8c764f775
Nadim Hairi <nadimhairi@hotmail.com>
2024-04-17 19:49:37 +0800

    add meta data

--- src/components/forms/buy-form.tsx
+++ src/components/forms/buy-form.tsx
@@ -4,7 +4,7 @@ import * as z from 'zod'
 import { useState, useTransition } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { notFound, useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { LoginSchema } from '@/schemas'
 import { Input } from '@/components/ui/input'
 import {
@@ -20,7 +20,7 @@ import { Button } from '@/components/ui/button'
 import { FormError } from '@/components/form-error'
 import { FormSuccess } from '@/components/form-success'
 import { login } from '@/actions/login'
-import { Copy, Loader2 } from 'lucide-react'
+import { ArrowLeft, Copy, Loader2 } from 'lucide-react'
 import {
   Card,
   CardContent,
@@ -31,6 +31,9 @@ import {
 import { Separator } from '../ui/separator'
 import MaxWidthWrapper from '../MaxWidthWrapper'
 import { Textarea } from '../ui/textarea'
+import Image from 'next/image'
+import { AspectRatio } from '../ui/aspect-ratio'
+import Link from 'next/link'
 
 type Product = {
   data: {
@@ -87,90 +90,123 @@ function BuyForm(product: Product) {
   }
   return (
     <MaxWidthWrapper>
-      <div className="mt-16 flex flex-wrap justify-center gap-4 gap-y-16 md:mt-32">
-        <CardWrapper header="Request Order" className="md:w-[450px]">
-          <Form {...form}>
-            <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
-              <div className="space-y-4">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Phone Number</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          disabled={isPending}
-                          placeholder="Enter your phone number"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Message (Optional)</FormLabel>
-                      <FormControl>
-                        <Textarea
-                          {...field}
-                          disabled={isPending}
-                          placeholder="I want to..."
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Location</FormLabel>
-                      <FormControl>
-                        <Textarea
-                          {...field}
-                          disabled={isPending}
-                          placeholder="ex: Room B-222, Blok A, Kolej 3..."
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+      <div className="mt-16 flex flex-wrap justify-center gap-4 gap-y-8 md:mt-32">
+        <div className="flex w-full flex-col gap-4 md:w-[450px]">
+          <CardWrapper className="hidden md:block md:w-[450px]">
+            <div className="flex items-center gap-4">
+              <Link href={'/product/' + product.data.id}>
+                <ArrowLeft />
+              </Link>
+              <div className="w-20">
+                <AspectRatio ratio={1 / 1}>
+                  <Image
+                    src={product.data.thumbnail!}
+                    alt={product.data.title!}
+                    fill
+                    className="rounded-md object-cover"
+                  />
+                </AspectRatio>
               </div>
-              <FormError message={error} />
-              <FormSuccess message={success} />
-              <div className="fixed inset-x-0 bottom-0 z-20 w-full flex-none lg:static lg:w-auto">
-                <Card className="p-4 shadow-[0_0_60px_-15px_rgba(0,0,0,0.3)] lg:border-none lg:px-0 lg:shadow-none">
-                  {isPending ? (
-                    <Button disabled className="w-full">
-                      <Loader2 className="h-4 w-4 animate-spin" />
-                    </Button>
-                  ) : (
-                    <Button
-                      disabled={isPending}
-                      type="submit"
-                      className="w-full"
-                    >
-                      Purchase
-                    </Button>
-                  )}
-                  <CardDescription className="pt-3 text-center lg:text-left">
-                    By tapping on &apos;Purchase&apos;, you accept the
-                    <span className="text-blue-400"> terms & conditions </span>
-                    from Stooket
-                  </CardDescription>
-                </Card>
+              <div>
+                <h1 className="w-full truncate font-medium">
+                  {product.data.title}
+                </h1>
+                <p>RM{product.data.price?.toFixed(2)}</p>
+                <p className="text-sm">@{product.data.username}</p>
               </div>
-            </form>
-          </Form>
-        </CardWrapper>
+            </div>
+          </CardWrapper>
+          <CardWrapper header="Request Order" className="md:w-[450px]">
+            <Form {...form}>
+              <form
+                className="space-y-6"
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                <div className="space-y-4">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Phone Number</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            disabled={isPending}
+                            placeholder="Enter your phone number"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Message (Optional)</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            {...field}
+                            disabled={isPending}
+                            placeholder="I want to..."
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Location</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            {...field}
+                            disabled={isPending}
+                            placeholder="ex: Room B-222, Blok A, Kolej 3..."
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormError message={error} />
+                <FormSuccess message={success} />
+                <div className="fixed inset-x-0 bottom-0 z-20 w-full flex-none lg:static lg:w-auto">
+                  <Card className="p-4 shadow-[0_0_60px_-15px_rgba(0,0,0,0.3)] lg:border-none lg:px-0 lg:shadow-none">
+                    {isPending ? (
+                      <Button disabled className="w-full">
+                        <Loader2 className="h-4 w-4 animate-spin" />
+                      </Button>
+                    ) : (
+                      <Button
+                        disabled={isPending}
+                        type="submit"
+                        className="w-full"
+                      >
+                        Submit Your Order
+                      </Button>
+                    )}
+                    <CardDescription className="pt-3 text-center lg:text-left">
+                      By tapping on &apos;Submit Your Order&apos;, you accept
+                      the
+                      <span className="text-blue-400">
+                        {' '}
+                        terms & conditions{' '}
+                      </span>
+                      from Stooket
+                    </CardDescription>
+                  </Card>
+                </div>
+              </form>
+            </Form>
+          </CardWrapper>
+        </div>
         <Card className="z-10 h-full w-[90%] overflow-hidden md:w-80 md:max-w-96">
           <CardHeader className="flex flex-row items-start bg-muted/50">
             <div className="grid gap-0.5">
